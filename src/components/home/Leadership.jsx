b3813842e35dcc0de832953cfec2a4e309663d86
Danial Asaria <danialasaria@danials-mbp.lan>
2020-12-26 18:18:07 -0800

    general updates

--- src/components/home/Leadership.jsx
+++ src/components/home/Leadership.jsx
@@ -32,7 +32,7 @@ const Leadership = () => {
                     and since then have become a 5-time National Champion and am currently the #1 18-year-old on the west coast of the United States. In addition, 
                     I have defeated numerous world-renowned Grandmasters and played some of the best in the world including World Champion Viswanathan Anand and Grandmaster Hikaru 
                     Nakamura. My peak United States Chess Federation and International Chess Federation ratings are 2420 and 2340 respectively, awarding me the titles of 
-                    National Master and FIDE International Master (top .43% in the world Inspired to share this passion I created the Asaria School of Chess where I teach students ages 5-80 from across the US, UK, and Asia.{/* I've been extremely grateful to have opportunities to lead my peers
+                    FIDE International Master and National Master/Life Master (top .43% in the world). Inspired to share this passion I created the Asaria School of Chess where I teach students ages 5-80 from across the US, UK, and Asia.{/* I've been extremely grateful to have opportunities to lead my peers
                        */}
                  </p>  
               </div>
